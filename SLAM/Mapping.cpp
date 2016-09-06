b2d3b45b6d1b20d89887a93ef6053894ea96a2c5
NBartlett25 <Nathan.Bartlett@uon.edu.au>
2016-09-06 12:37:32 +1000

    Added Other Variables from Matlab Date: 06/09/16

--- SLAM/Mapping.cpp
+++ SLAM/Mapping.cpp
@@ -3,6 +3,7 @@
 Mapping::Mapping(Map<MatrixXd> currentgrid, double* params)
 {
 	grid = currentgrid;
+	gridy = MatrixXd::Zero(grid.rows(), grid.cols());
 	dec = params[0];
 	inc = params[1];
 	max = params[2];
@@ -13,12 +14,14 @@ Mapping::Mapping(Map<MatrixXd> currentgrid, double* params)
 
 void Mapping::Nav() {
 	
+
+	  rad, scan, cells, map
 }
-/*void Mapping::Grid(MatrixXd rnCN, MatrixXd RnCN, MatrixXd QC, VectorXd ocam, VectorXd radii, int numObstacles) {
+void Mapping::Grid(MatrixXd rnCN, MatrixXd RnCN, MatrixXd QC, MatrixXd cells, RowVectorXd cam, VectorXd radii, int numObstacles) {
 	//MatrixXd rnCN(3, 4), MatrixXd RnCN(3, 12), MatrixXd QC(4, 255), VectorXd ocam(255), VectorXd radii(255)
 	// Decided on 255 -> Large Number to hopefully contain all obstacles seen in timestep. (Can be larger, or make dynamic)
 	// Have int index telling us how big this matrices are for current timestep (from length of camera data) (nuObstacles)
-}*/
+}
 void Mapping::MeasureObs() {
 	//Takes in all
 }
@@ -37,6 +40,7 @@ void Mapping::MeasureObs() {
  */
 void Mapping::MeasureLand(/*rnCN,RnCN,rnBN,RnBN,LC,cam,lmrks*/) {
 
+	/*
 	MatrixXd LC(1,1); // Initialisation value of LC to detect when there are no landmark observations
 	
 	VectorXd bearingy,
@@ -45,7 +49,7 @@ void Mapping::MeasureLand(/*rnCN,RnCN,rnBN,RnBN,LC,cam,lmrks*/) {
 			 rangeyhat;
 	/*********************************************************************************************************************/
 	/* Camera Observation */
-
+	/*
 	if (LC.rows() > 1) {  // This condition assumes that LC will be initialised to a 1x1 matrix if there is no landmarks
 		MatrixXd rcLC = LC.topRows(3);
 		VectorXd thLC = LC.bottomRows(1);
@@ -68,7 +72,7 @@ void Mapping::MeasureLand(/*rnCN,RnCN,rnBN,RnBN,LC,cam,lmrks*/) {
 
 	/**********************************************************************************************************************/
 	/* GPS and IMP Approximation with Assumed GPS Landmark Location */
-
+	/*
 	if (lmrks.rows() > 1) {  // This condition assumes that lmrks will be initialised to a 1x1 matrix if there is no landmarks
 		MatrixXd local = RnBN*(lmrks - rnBN.replicate(1, lmrks.rows()));
 		bearingyhat = atan2(local.row(1), local.row(0));  // unsure how atan2 is going to handle this
@@ -77,7 +81,7 @@ void Mapping::MeasureLand(/*rnCN,RnCN,rnBN,RnBN,LC,cam,lmrks*/) {
 
 	/**********************************************************************************************************************/
 	/* Form Outputs */
-
+	/*
 	if (lmrks.rows() > 1) {
 		VectorXd y(rangey.rows(),rangey.cols()+bearingy.cols());
 		VectorXd yhat(rangeyhat.rows(),rangeyhat.cols()+bearingyhat.cols());
@@ -91,10 +95,13 @@ void Mapping::MeasureLand(/*rnCN,RnCN,rnBN,RnBN,LC,cam,lmrks*/) {
 		yhat << 0;
 
 	}
+	*/
 }
 
-MatrixXd Mapping::GetGrid() {
-	return grid;
+MatrixXd Mapping::GetGrid(bool flag) {
+	MatrixXd output;
+	output = (flag) ? gridy : grid;
+	return output;
 }
 
 VectorXd Mapping::GetObsVect() {
--- SLAM/main.cpp
+++ SLAM/main.cpp
@@ -8,21 +8,30 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
 {
 	/*Initialise Mapping Class Parameters*/
 
-	MatrixXd outgrid;
+	MatrixXd grid, gridy;
 	Map<MatrixXd> gridcurrent(mxGetPr(prhs[0]), mxGetM(prhs[0]), mxGetN(prhs[0]));
 	Mapping MapObject(gridcurrent, mxGetPr(prhs[1]));
 
 	/*Obtain All Other Variables from Matlab*/
 
+	Map<MatrixXd> rnCN(mxGetPr(prhs[2]), 3, 4);
+	Map<MatrixXd> RnCN(mxGetPr(prhs[3]), 3, 12);
+	Map<MatrixXd> QC(mxGetPr(prhs[4]), mxGetM(prhs[4]), mxGetN(prhs[4]));
+	Map<MatrixXd> cells(mxGetPr(prhs[5]), mxGetM(prhs[4]), mxGetN(prhs[5]));
+	Map<RowVectorXd> cam(mxGetPr(prhs[6]), mxGetN(prhs[6]));
+	//Map<RowVectorXd> scan(mxGetPr(prhs[7]), mxGetN(prhs[7])); DONT NEED THIS!
+
 	/*Call C++ Functions*/
 
-	/*Outputs*/
 
-	outgrid = MapObject.GetGrid();
-	plhs[0] = mxCreateDoubleMatrix((mwSize)outgrid.rows(), (mwSize)outgrid.cols(), mxREAL);
-	Map<MatrixXd>(mxGetPr(plhs[0]), outgrid.rows(), outgrid.cols()) = outgrid;
+
+	/*Outputs for Grid Function*/
+
+	grid = MapObject.GetGrid(0);
+	gridy = MapObject.GetGrid(1);
+	plhs[0] = mxCreateDoubleMatrix((mwSize)grid.rows(), (mwSize)grid.cols(), mxREAL);
+	Map<MatrixXd>(mxGetPr(plhs[0]), grid.rows(), grid.cols()) = grid;
 	
 
-	
 	
 }
